4e186af5d5257b1e826a15645e9dd888608286da
cl4ram <molinayvediaclara@outlook.com>
2024-06-24 15:15:48 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# React notes app - Learn React Project from Scrimba
+
+This is a project for the 'Learn React' course from Scrimba.
+
+## Screenshot
+
+![](./screenshot.png)
+
+## Links
+
+- Live Site URL: 
+
+## My process
+
+- useState and useEffect
+- Conditional rendering
+- React mde
+
+## To-do
+
+- Implement Firebase
+- Update styles
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+/* eslint-disable react/prop-types */
+export default function Sidebar(props) {
+    const noteElements = props.notes.map((note) => (
+        <div key={note.id} className='note'>
+            <div
+                className={`title ${
+                    note.id === props.currentNote.id ? 'selected-note' : ''
+                }`}
+                onClick={() => props.setCurrentNoteId(note.id)}
+            >
+                <h4 className='text-snippet'>{note.body.split('\n')[0]}</h4>
+                <button 
+                    className='delete-btn'
+                    onClick={(event) => props.deleteNote(event, note.id)}
+                >
+                    <i className='bi bi-trash'></i>
+                </button>
+
+            </div>
+        </div>
+    ))
+
+    return (
+        <section className='pane sidebar'>
+            <div className='sidebar--header'>
+                <h3>Notes</h3>
+                <button className='new-note' onClick={props.newNote}>+</button>
+            </div>
+            {noteElements}
+        </section>
+    )
+}
